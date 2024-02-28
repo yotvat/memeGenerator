49703bf0041e389e2b051450769f8db980117588
yotvat <idooy11@gmail.com>
2024-02-28 12:42:53 +0200

    Render an img on the canvas and add txt

--- js/memeController.js
+++ js/memeController.js
@@ -1,1 +1,60 @@
 'use strict'
+let gElCanvas
+let gCtx
+
+function onInit() {
+    gElCanvas = document.querySelector('canvas')
+    gCtx = gElCanvas.getContext('2d')
+    renderMeme()
+
+
+    //window.addEventListener('resize', () => resizeCanvas())
+}
+
+function renderMeme() {
+    const img = new Image()
+    img.src = getImg(1)
+    img.onload = () => {
+        gElCanvas.height = (img.naturalHeight / img.naturalWidth) * gElCanvas.width
+        gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
+        drawText(gMeme.lines[0].txt,100,100)
+    }
+
+
+
+
+
+    // console.log(elImg.src)
+    // gElCanvas.height = (elImg.naturalHeight / elImg.naturalWidth) * gElCanvas.width
+    // gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
+}
+
+function onSetLineChange({value}){
+    setLineTxt(value)
+    renderMeme()
+}
+
+function drawText(text, x, y) {
+	gCtx.lineWidth = 2
+	gCtx.strokeStyle = 'black'
+
+	gCtx.fillStyle = 'white'
+
+	gCtx.font = '45px Arial'
+	gCtx.textAlign = 'center'
+	gCtx.textBaseline = 'middle'
+
+	gCtx.fillText(text, x, y)
+	gCtx.strokeText(text, x, y)
+}
+
+
+
+
+
+//function resizeCanvas() {
+    // const elContainer = document.querySelector('.canvas-container')
+    
+    // // Changing the canvas dimension clears the canvas
+    // gElCanvas.width = elContainer.clientWidth
+// }
--- services/memeService.js
+++ services/memeService.js
@@ -1,1 +1,25 @@
 'use strict'
+
+var gImg = [{ id: 1, url: 'img/1.jpg', keywords: ['funny', 'president'] }]
+var gMeme = {
+    selectedImgId: 1,
+    selectedLineIdx: 0,
+    lines: [
+        {
+            number: 1,
+            txt: 'MAGA',
+            size: 20,
+            color: 'green'
+        }
+    ]
+}
+var gKeywordSearchCountMap = { 'funny': 4, 'presiden': 2 }
+
+function getImg(id) {
+    return gImg.find(Img => Img.id === id).url
+}
+
+function setLineTxt(value) {
+    gMeme.lines[0].txt = value
+    console.log(gMeme.lines[0].txt);
+}
