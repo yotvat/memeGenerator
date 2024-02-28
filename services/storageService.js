c43ec9ba706169acf66e0d7635808568f18c18e8
yotvat <idooy11@gmail.com>
2024-02-28 11:26:10 +0200

    Initial commint

---
+++ services/storageService.js
@@ -0,0 +1,11 @@
+'use strict'
+
+function saveToStorage(key, value) {
+    const valueStr = JSON.stringify(value)
+    localStorage.setItem(key, valueStr)
+}
+
+function loadFromStorage(key) {
+    const valueStr = localStorage.getItem(key)
+    return JSON.parse(valueStr)
+}
---
+++ services/utilService.js
@@ -0,0 +1,21 @@
+'use strict'
+
+function makeId(length = 5) {
+    let txt = ''
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    for (let i = 0; i < length; i++) {
+        txt += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
+    return txt
+}
+
+function getFormattedDate(time) {
+    const date = new Date(time)
+    return date.toDateString()
+}
+
+function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled)
+}
